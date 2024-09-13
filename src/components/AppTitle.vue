f53a78135d2e8cedf53d193cc1bc4a11ab06a3ad
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-13 17:22:11 +0200

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data()  {
+        return {
+            
+        }
+    }
+}
+</script>
+
+
+<template>
+<h1>La mia prima App con Vite!</h1>
+</template>
+
+<style scoped>
+
+</style>
